348ae3dcd2894feb2b654f4a93de511981e17fc9
Сергей <gan7er@yandex.ru>
2021-02-22 16:22:53 +0300

    Справочник

---
+++ Lesson-3-2/Lesson-3-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[,] spravker = { { "Sergey", "121212" }, { "Ivan", "232323" }, { "Vovan", "323232" }, { "Vasya", "454545" }, { "Katya", "kat@mail.ru" } };
+            //Вывести справочник при нажатии
+            Console.WriteLine("Нажмите клавишу для выведения списка контактов");
+            Console.ReadKey();
+            Console.Clear();
+
+            for (int i = 0; i < spravker.GetLength(0); i++)
+            {
+                Console.WriteLine($"{spravker[i, 0]}:{spravker[i, 1]}");
+            }
+        }
+    }
+}
