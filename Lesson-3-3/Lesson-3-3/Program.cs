f03d1eda61c24bc327059aab73ba39110199a2ec
Сергей <gan7er@yandex.ru>
2021-02-22 16:31:10 +0300

    Вариант 1

---
+++ Lesson-3-3/Lesson-3-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово, чтобы вывести его наоборот ");
+            string a = Console.ReadLine();
+            string c = "";
+            char[] b = new char[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                b[i] = a[i];
+            }
+            
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+
+                c += b[i];
+
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
